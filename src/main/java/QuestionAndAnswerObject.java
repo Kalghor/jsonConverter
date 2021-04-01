771c250d0c7c8e6ad74281f0f12883c0b95b16a2
Paweł Barański <kalghor@gmail.com>
2021-04-01 16:48:27 +0200

    adding a class representing a question with an answer

---
+++ src/main/java/QuestionAndAnswerObject.java
@@ -0,0 +1,10 @@
+public class QuestionAndAnswerObject {
+
+    private String question;
+    private String answer;
+
+    public QuestionAndAnswerObject(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
